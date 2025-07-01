655c1ce4ee148765b63e6fae8440ae4eaf8f07c8
anonimo3050 <141656094+anonimo3050@users.noreply.github.com>
2025-07-01 15:19:09 -0500

    subiendo página

---
+++ script.js
@@ -0,0 +1,9 @@
+let header = document.querySelector("header")
+
+window.addEventListener("scroll", () => {
+    if(window.scrollY > 190){
+        header.classList.add("fijo")
+    }else{
+        header.classList.remove("fijo")
+    }
+})
